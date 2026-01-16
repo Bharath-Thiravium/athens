e605f48fb68384d8bc93edd9c5ce2852687fd0c2
Athens Admin <admin@athenas.co.in>
2026-01-16 12:41:19 +0000

    Add attendance events bulk API and training codes

---
+++ app/backend/backend/urls.py
@@ -0,0 +1,58 @@
+"""
+URL configuration for backend project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include, re_path
+from django.contrib import admin
+from django.views.generic import RedirectView
+from .health import health_check
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from rest_framework.routers import DefaultRouter
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('health/', health_check, name='health_check'),
+    path('authentication/', include('authentication.urls')),
+    path('api/menu/', include('authentication.menu_urls')),  # Menu endpoints
+    path('api/attendance/', include('attendance.urls')),
+    # path('', include('authentication.tenant_urls')),  # Tenant management APIs - Disabled due to missing serializer
+    # path('', include('authentication.menu_urls')),  # Menu management APIs - Disabled
+    path('chatbox/', include('chatbox.urls')),
+    path('worker/', include('worker.urls')),
+    path('tbt/', include('tbt.urls')),
+    path('induction/', include('inductiontraining.urls')),
+    path('jobtraining/', include('jobtraining.urls')),
+    path('man/', include('manpower.urls')),
+    path('', include('mom.urls')),
+    path('api/v1/safetyobservation/', include('safetyobservation.urls')),
+    path('api/v1/ptw/', include('ptw.urls')),
+    path('api/v1/incidentmanagement/', include('incidentmanagement.urls')),
+    path('api/v1/inspection/', include('inspection.urls')),
+    path('api/v1/permissions/', include('permissions.urls')),
+    path('system/', include('system.urls')),
+    path('api/v1/environment/', include('environment.urls')),
+    path('api/v1/quality/', include('quality.urls')),
+    path('api/v1/voice/', include('voice_translator.urls')),
+    path('api/control-plane/', include('control_plane.urls')),
+    path('api/saas/', include('control_plane.saas_urls')),
+    # path('api/v1/translation/', include('apps.translation.urls')),  # Commented out due to httpcore compatibility issue
+    path('', RedirectView.as_view(url='/admin/', permanent=True)),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
